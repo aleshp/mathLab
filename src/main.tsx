f648e65987f51a00255c41f4fbb5aef6a57ef1c9
Alesh <94743489+aleshp@users.noreply.github.com>
2025-12-25 23:45:35 +0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import 'katex/dist/katex.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// === ВАЖНОЕ ИСПРАВЛЕНИЕ ДЛЯ MATHQUILL ===
+// 1. Импортируем jQuery
+import $ from 'jquery';
+
+// 2. Делаем его глобальным (MathQuill ищет его именно так)
+// @ts-ignore
+window.jQuery = $;
+// @ts-ignore
+window.$ = $;
+// @ts-ignore
+window.global = window; // Страховка для первой ошибки
+// ========================================
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>,
+)
