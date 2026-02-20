9e0af94efb1cbf062173ff75ab5de2f1d52ca133
Alesh <94743489+aleshp@users.noreply.github.com>
2026-02-21 02:35:51 +0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import './i18n';
 
 // Важно: просто импортируем стили/библиотеку, но НЕ отключаем её глобально
 import 'mathlive';
