3dcde2a9b6a398c61f07014ebb33525691f80e72
Alesh <94743489+aleshp@users.noreply.github.com>
2026-03-05 06:31:59 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react" 
 import './i18n';
-
-// Важно: просто импортируем стили/библиотеку, но НЕ отключаем её глобально
 import 'mathlive';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>,
 )
